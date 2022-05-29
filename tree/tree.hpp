1c52274966eeb0bfde7ec7703a1d366d88f0325a
p4v31 <90404785+p4v31@users.noreply.github.com>
2022-05-29 18:33:45 +0300

    Update tree.hpp

--- tree/tree.hpp
+++ tree/tree.hpp
@@ -1,14 +1,21 @@
+#include <iostream>
 #include <memory>
 
 struct tree_node {
   int value;
   tree_node *up;
   std::unique_ptr<tree_node> left, right;
+
+  friend auto operator<<(std::ostream &out, tree_node &node) -> std::ostream &;
 };
 
 struct tree {
   std::unique_ptr<tree_node> root;
 
+  auto find(int val) -> tree_node *;
+  auto find_successor(tree_node *node) -> tree_node *;
   auto insert(int val) -> tree_node *;
   auto remove(int val) -> bool;
+  auto delete_node(std::unique_ptr<tree_node> node, int val) -> std::unique_ptr<tree_node>;
+  friend auto operator<<(std::ostream &out, tree &tree) -> std::ostream &;
 };
